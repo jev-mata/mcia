a420954dd6309f15b2bc8c1f45e36f6afaa8874a
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-03-11 12:42:39 +0800

    Fullscreen

--- mcia.bat
+++ mcia.bat
@@ -1,11 +1,11 @@
 ::[Bat To Exe Converter]
 ::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJHOB4n49KxJVDAGaOTmGK5Y1yaj+9++7r04UVd4sc4bV5rWdM+UH+XntdpkjxUZcmd0FDxRWMBuoYW8=
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
+::cxAkpRVqdFKZSzk=
 ::cBs/ulQjdF+5
 ::ZR41oxFsdFKZSDk=
 ::eBoioBt6dFKZSDk=
@@ -13,14 +13,14 @@
 ::egkzugNsPRvcWATEpCI=
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXrZg==
+::ZQ05rAF9IAHYFVzEqQIdCzJ1DEnCCmKoEqAd5KjP4P+Vwg==
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmhFUr6S/Uq4frv+uuL4nkSQfBf
+::fBEirQZwNQPfEVWB+kM9LVsJDCmhFUr6S/Uq4frv+uuL4nkSQfBf
+::cRolqwZ3JBvQF1fEqQIdCzJ1DEnCCmKoEqAd5KjP4P+Vwg==
+::dhA7uBVwLU+EWHSF4ENwHB5XRAqNMGS9D7APqMH17KTn
 ::YQ03rBFzNR3SWATElA==
 ::dhAmsQZ3MwfNWATElA==
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
